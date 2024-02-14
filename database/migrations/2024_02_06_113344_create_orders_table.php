f66d6d5b71fda44044f9b1e226763676c7bbcccf
Naif Mbarak <nabdalla745@gmail.com>
2024-02-14 11:50:02 +0300

    Changed total column to be of type decimal and status to be an enum

--- database/migrations/2024_02_06_113344_create_orders_table.php
+++ database/migrations/2024_02_06_113344_create_orders_table.php
@@ -1,22 +1,23 @@
 <?php
 
+use App\Enums\OrderStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up()
+    public function up():void
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('total');
-            $table->string('status');
+            $table->decimal('total');
+            $table->enum('status', OrderStatus::values())->default(OrderStatus::UNPAID->value);
             $table->foreignId('user_id')->constrained()->nullOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
 
-    public function down()
+    public function down():void
     {
         Schema::dropIfExists('orders');
     }
