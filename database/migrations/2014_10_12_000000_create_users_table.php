1994c7b64de033836f1ec74b9224c9fd1b0abf81
Naif Mbarak <nabdalla745@gmail.com>
2024-02-22 11:57:06 +0300

    Added role column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('password');
             $table->date('birthday')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
